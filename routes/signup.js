794df6cbe7ec485b35dd4ceed94ef43b1de9fdf4
rahulgoya030 <goyal.rahul030@gmail.com>
2015-09-03 22:18:42 +0530

    signup db added

--- routes/signup.js
+++ routes/signup.js
@@ -3,15 +3,16 @@ var express = require('express');
 var mongodb = require('mongodb');
 
 var MongoClient= mongodb.MongoClient;
-var url = 'mongodb://localhost:27017/test1';
+var url = 'mongodb://localhost:27017/twitter+';
 
 var router = express.Router();
 
 router.post('/', function  (req,res) {
 
-	console.log(req.url , " "  , req.body);
+	//console.log(req.url , " "  , req.body.username1 , req.body.email , req.body.pass);
+
 	var MongoClient= mongodb.MongoClient;
-	var url = 'mongodb://localhost:27017/test1';
+	var url = 'mongodb://localhost:27017/twitter+';
 
 	MongoClient.connect(url, function (err,db){
 
@@ -22,6 +23,24 @@ router.post('/', function  (req,res) {
 
 		console.log("connected to the server");
 
+		var username = req.body.username1;
+		var email = req.body.email;
+		var password = 	req.body.pass;	
+
+		var data = { 'username' : username  , 'email' : email , 'password' : password }
+
+		var collection = db.collection('users');
+
+		collection.insert(data , function ( err, result){
+
+				if(err)
+				{
+					console.log(err);
+				}
+				console.log(" data inserted ");
+		})
+
+		//db.close();
 
 
 	})
