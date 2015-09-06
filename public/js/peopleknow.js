918d6bc1165c74fcf7969b74ad8b4e63521e30c9
rahulgoya030 <goyal.rahul030@gmail.com>
2015-09-07 02:47:45 +0530

    people u may know added

--- public/js/peopleknow.js
+++ public/js/peopleknow.js
@@ -1,5 +1,9 @@
+
+
 $(document).ready(function  () {
 	
+
+
 	console.log("hello");
     $.get("/peopleknow", function(data , status){
 
@@ -10,15 +14,34 @@ $(document).ready(function  () {
     			console.log(data[x].username);
 
     			var person= '<p >' +  data[x].username + "</p>";
-    			var followButton = '<button  > follow </button>';
+    			var followButton ='<button type="button"  class="myButton"   value ="' + data[x].username  + ' ">follow </button>';
     			
-    			$("#peopleList").after(followButton).after(person);
+
+    			$("#peopleList").append(person);
+
+    			$("#peopleList").append(followButton);    // correcting this for right value
 
 
     				
 
     		}	
 
+    });
+
+
+    $(".people p").click(function(e){
+
+    	e.preventDefault();
+           
+    	 console.log(" IN THIS  code")
+    	 var value=  $(this).html();
+    	 console.log(" button value is "  , value);
+    });
+
+    $(".myButton").click(function(){
+
+    	console.log(" it is responding");
     })
 
+    
 })
