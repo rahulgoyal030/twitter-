e2e626b9e7aad2fcdb5fbfc0fe5ffae96e74fba1
rahulgoya030 <goyal.rahul030@gmail.com>
2015-09-07 13:50:47 +0530

    tweets  added to db

--- routes/tweetSave.js
+++ routes/tweetSave.js
@@ -17,14 +17,30 @@ router.post('/', function  (req,res) {
 			 console.log(err);
 		}
 
-		console.log(" connected to the server");
+		console.log(" connected to the db");
 
 		 var username = req.body.username;
 
 		 var tableName = username+"_tweets";
 		 console.log(tableName , username.length);
 
-		// var collecction = db.collecction('')
+		  //db.createCollection(tableName);
+
+		  var collection = db.collection(tableName);
+
+		  var data = { 'tweet' : req.body.tweet  };
+		  console.log(data);
+		  var data
+		 	collection.insert( data , function ( err, result){
+
+					if(err)
+					{
+						console.log(err);
+					}
+					console.log(" data inserted ");
+			        
+			       
+			 });
 
 
 
